92a234f35427b27bb494dbc8c4b5cc8d2d8edc03
Us-Man-Qa-Sim <usmanqasim0900@gmail.com>
2026-04-19 18:28:58 +0500

    resolve-production-bug | bug has been resolved

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -1,13 +1,9 @@
-import { useGLTF } from '@react-three/drei';
 import { useRef } from 'react';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 
 const Target = (props) => {
   const targetRef = useRef();
-  const { scene } = useGLTF(
-    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf',
-  );
 
   useGSAP(() => {
     gsap.to(targetRef.current.position, {
@@ -19,9 +15,38 @@ const Target = (props) => {
   });
 
   return (
-    <mesh {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
-      <primitive object={scene} />
-    </mesh>
+    <group {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
+      {/* Stand pole */}
+      <mesh position={[0, -0.6, 0]}>
+        <cylinderGeometry args={[0.04, 0.04, 1.2, 12]} />
+        <meshStandardMaterial color="#aaaaaa" metalness={0.6} roughness={0.4} />
+      </mesh>
+      {/* Base */}
+      <mesh position={[0, -1.2, 0]}>
+        <cylinderGeometry args={[0.35, 0.35, 0.06, 24]} />
+        <meshStandardMaterial color="#777777" metalness={0.5} roughness={0.5} />
+      </mesh>
+      {/* Outer red ring */}
+      <mesh rotation={[Math.PI / 2, 0, 0]}>
+        <torusGeometry args={[0.5, 0.12, 16, 64]} />
+        <meshStandardMaterial color="#cc2200" />
+      </mesh>
+      {/* Middle white ring */}
+      <mesh rotation={[Math.PI / 2, 0, 0]}>
+        <torusGeometry args={[0.32, 0.1, 16, 64]} />
+        <meshStandardMaterial color="#ffffff" />
+      </mesh>
+      {/* Inner red bullseye */}
+      <mesh position={[0, 0, 0.01]} rotation={[Math.PI / 2, 0, 0]}>
+        <circleGeometry args={[0.18, 32]} />
+        <meshStandardMaterial color="#cc2200" />
+      </mesh>
+      {/* Target face backing */}
+      <mesh position={[0, 0, -0.01]} rotation={[Math.PI / 2, 0, 0]}>
+        <circleGeometry args={[0.64, 32]} />
+        <meshStandardMaterial color="#f0f0f0" />
+      </mesh>
+    </group>
   );
 };
 
